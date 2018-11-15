c75c2777cc3fa04c13b5117d142b2a298b3e8a85
ritchey0713 <david@osa.io>
2018-11-15 16:49:27 -0500

    add new tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_15_203235) do
+ActiveRecord::Schema.define(version: 2018_11_15_213446) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string "tag"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.integer "user_id"
+    t.string "title"
+    t.string "subtitle"
+    t.string "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "posts_categories", id: false, force: :cascade do |t|
+    t.integer "post_id"
+    t.integer "category_id"
+    t.index ["category_id"], name: "index_posts_categories_on_category_id"
+    t.index ["post_id"], name: "index_posts_categories_on_post_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "user_name"
