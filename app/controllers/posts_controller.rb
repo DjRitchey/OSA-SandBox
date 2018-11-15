60fa0cf05affbf4a6da86253d9fa89672151eb04
ritchey0713 <david@osa.io>
2018-11-15 17:08:10 -0500

    add post.all

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,7 +1,28 @@
 class PostsController < ApplicationController
 
   def index
+    @posts = Post.all
+  end
 
+  def new
+    @user = current_user
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render new_post_path
+    end
+  end
+
+
+private
+
+  def post_params
+    params.require(:post).permit(:user_id, :title, :subtitle, :content)
   end
 
 end
