cf8743b448df3258a3c8d505b30c0dc774f7da69
ritchey0713 <david@osa.io>
2018-11-16 12:04:05 -0500

    add phone validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,5 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable, :confirmable, :lockable
   validates :user_name, presence: true
+  validates :phone, phone: { possible: false }
+
+
   has_many :posts
 end
