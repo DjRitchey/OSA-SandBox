6afc3c642988aa37ada984b9f58ca3c8aace4b1f
ritchey0713 <david@osa.io>
2018-11-16 12:04:57 -0500

    debug invalidity of phone numbers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,7 +2,7 @@ class UsersController < ApplicationController
   def index
     @users = User.all
     respond_to do |format|
-      format.html { render action: "listing" }
+      format.html 
       format.json
     end
   end
